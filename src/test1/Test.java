8eff74ee0afbfcab69f92bf1babea02d6d240c31
setmeonfire <51151871+setmeonfire@users.noreply.github.com>
2019-06-01 20:44:40 +0800

    github update 01

--- src/test1/Test.java
+++ src/test1/Test.java
@@ -1,9 +1,10 @@
 package test1;
-
+//first
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("aaa");
 		System.out.println("abc");
+		System.out.println("123");
 	}
 }
  
